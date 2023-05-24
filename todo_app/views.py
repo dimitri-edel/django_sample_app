83c6283418dc440e2fab396527b74cc2d1061004
Dimitri Edel <dimitri.edel@gmx.net>
2023-05-24 14:24:42 +0200

    Add query for Items in views and update the todo_list.html to show results of query

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, 'todo_app/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items' : items
+    }
+    return render(request, 'todo_app/todo_list.html', context=context)
