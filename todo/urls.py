c8e1059edb1aa92e924fa762b26f6e12442cb674
Dimitri Edel <dimitri.edel@gmx.net>
2023-05-24 13:17:14 +0200

    Create template folder and add todo_list.html

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from todo_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_todo_list, name='get_todo_list')
 ]
