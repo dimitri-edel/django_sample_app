6a7a3d07742e68e0c9772620248458762c04fad1
Dimitri Edel <dimitri.edel@gmx.net>
2023-05-24 17:39:03 +0200

    Create ItemForm and add it to view and template

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import ItemForm
 
 # Create your views here.
 def get_todo_list(request):
@@ -10,5 +11,14 @@ def get_todo_list(request):
     return render(request, 'todo_app/todo_list.html', context=context)
 
 def add_item(request):
-    
-    return render(request, 'todo_app/add_item.html')
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+
+    form = ItemForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'todo_app/add_item.html', context=context)
